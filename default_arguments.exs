f6b86ac8f4de1d3ceaf28a73ee2ccfd48637c13b
Kohei TAKATA <kt.koheitakata@gmail.com>
2015-05-27 08:38:55 +0900

    -Modules

---
+++ default_arguments.exs
@@ -0,0 +1,15 @@
+defmodule Concat do
+  def join(a, b \\ nil, sep \\ " ")
+
+  def join(a, b, _sep) when is_nil(b) do
+    a
+  end
+
+  def join(a, b, sep) do
+    a <> sep <> b
+  end
+end
+
+IO.puts Concat.join("Hello", "World")
+IO.puts Concat.join("Hello", "World", "_")
+IO.puts Concat.join("Hello")
